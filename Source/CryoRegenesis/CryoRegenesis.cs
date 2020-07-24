04bee363fe3551ca85a5ab95b0842930eee534c5
Theodore R. Smith <theodore@phpexperts.pro>
2020-07-23 23:28:18 -0500

    Lots and lots of improvements that I'm pretty happy with.

--- Source/CryoRegenesis/CryoRegenesis.cs
+++ Source/CryoRegenesis/CryoRegenesis.cs
@@ -7,12 +7,16 @@ namespace CryoRegenesis
 {
     public class Building_CryoRegenesis : Building_CryptosleepCasket
     {
+        private Random rnd = new Random();
+
         int cryptoHediffCooldown;
         int cryptoHediffCooldownBase = GenDate.TicksPerMonth / 2;
-        long restoreCoolDown = 4294967295;
+        long restoreCoolDown = -1000;
         int enterTime;
-        int rate = 30;
-        int fuelConsumption = 20;
+        //int rate = 30;
+        //int rate = 150;
+        int rate = 1500;
+        float fuelConsumption;
         HediffDef cryosickness = HediffDef.Named("CryptosleepSickness");
         CompRefuelable refuelable;
         CompPowerTrader power;
@@ -67,6 +71,15 @@ namespace CryoRegenesis
             props = power.Props;
             fuelprops = refuelable.Props;
             fuelprops.fuelConsumptionRate = fuelConsumption / 60f;
+            fuelConsumption = (refuelable.Props.fuelCapacity * fuelprops.fuelConsumptionRate) / GenDate.TicksPerYear;
+
+            //fuelprops.fuelConsumptionRate = 0.33333f;
+            Log.Message("Fuel capacity: " + refuelable.Props.fuelCapacity);
+            Log.Message("Fuel capacity v2: " + fuelprops.fuelCapacity);
+            Log.Message("Fuel consumption rate: " + fuelprops.fuelConsumptionRate);
+            Log.Message("Fuel consumption rate v2: " + fuelprops.fuelConsumptionRate);
+            Log.Message("Fuel consumption per Year: " + (refuelable.Props.fuelCapacity * refuelable.Props.fuelConsumptionRate));
+            Log.Message("Fuel consumption per Tick: " + fuelConsumption);
         }
 
         public override void ExposeData()
@@ -75,49 +88,127 @@ namespace CryoRegenesis
             Scribe_Values.LookValue<int>(ref cryptoHediffCooldown, "cryptoHediffCooldown");
             Scribe_Values.LookValue<int>(ref enterTime, "enterTime");
         }
+
+        private int CalculateHealingTime(Pawn pawn)
+        {
+            // Get the pawn's age in Years. e.g., 65 years.
+            int pawnAge = (int) (pawn.ageTracker.AgeBiologicalTicks / GenDate.TicksPerYear);
+
+            // If the pawn is age 25 or younger, set it for a year or less.
+            if (pawnAge <= 25) {
+                return GenDate.TicksPerYear / rnd.Next(1, 4);
+            }
+
+            // Get the decade. e.g., 7th decade
+            int decadeOfLife = (pawnAge / 10) + 1;
+
+            // 10  =   ?? - 10    = 10
+            //  9  =   11 -  9      20
+            //  8  =   11 -  8      30
+            //  7  =   11 -  7      40
+            //  6  =   11 -  6      50
+            //  5  =   11 -  5      60
+            //  4  =   11 -  4      70
+            //  3  =   11 -  3      80
+            //  2  =   11 -  2      90
+            //  1  =   11 -  1   = 100
+
+            int baseFrequency = (11 - decadeOfLife) * 10;
+            // E.g., if decade = 8, base = 30, min = 30 * (30/100) = 9
+            // E.g., if decade = 4, base = 70, min = 70 * (70/100) = 49
+            int minFrequency = (int)((double)baseFrequency * (double)baseFrequency / 100);
+            // E.g., if decade = 8, base = 30, max = 30 * ((30+100) / 100) = 39
+            // E.g., if decade = 4, base = 70, max = 70 * ((70+100) / 100) = 119
+            int maxFrequency = (int)((double)baseFrequency * (((double)baseFrequency + 100) / 100));
+
+            double frequency = rnd.Next(minFrequency, maxFrequency);
+
+            Log.Message("Healing Frequency: Base (" + baseFrequency + ") Min (" + minFrequency + ") Max (" + maxFrequency + ") Actual: " + frequency + "%");
+
+            return (int)Math.Round((frequency / 100) * (GenDate.TicksPerYear * decadeOfLife));
+        }
+
         public override void Tick()
         {
+            bool is21OrYounger;
             if (HasAnyContents && refuelable.HasFuel)
             {
                 Pawn pawn = ContainedThing as Pawn;
 
-                // Remove all health-related injuries if they're younger than age 21.
-                if (restoreCoolDown > 0 &&  pawn.ageTracker.AgeBiologicalTicks <= GenDate.TicksPerYear * 24)
+                if (power.PowerOn)
                 {
-                    // Heal brain injuries
-                    if (power.PowerOn)
+                    bool hasInjuries;
+
+                    long ticksLeft = (pawn.ageTracker.AgeBiologicalTicks - restoreCoolDown);
+                    double targetAge = (double)restoreCoolDown / (double)GenDate.TicksPerYear;
+
+                    is21OrYounger = pawn.ageTracker.AgeBiologicalTicks <= ((GenDate.TicksPerYear * 21) + rate);
+                    hasInjuries = (pawn.health.hediffSet.GetHediffs<Hediff>().Count() > 0);
+
+                    if (is21OrYounger && !hasInjuries)
                     {
-                        long ticksLeft = (pawn.ageTracker.AgeBiologicalTicks - restoreCoolDown);
-                        refuelable.ConsumeFuel(fuelConsumption / GenDate.TicksPerYear);
-                        if (ticksLeft % (rate * 100) == 0)
-                        {
-                            Log.Message("Restore Cooldown: " + ticksLeft);
-                        }
+                        //this.props.basePowerConsumption = 0;
+                        power.PowerOn = false;
+                        power.PowerOutput = 0;
 
-                        if (restoreCoolDown >= pawn.ageTracker.AgeBiologicalTicks)
+                        return;
+                    }
+
+                    refuelable.ConsumeFuel(fuelConsumption);
+                    //if (pawn.ageTracker.AgeBiologicalTicks % GenDate.TicksPerSeason == 0)
+                    if (power.PowerOn && hasInjuries && !is21OrYounger && pawn.ageTracker.AgeBiologicalTicks % GenDate.TicksPerSeason <= rate)
+                    {
+                        Log.Message("(" + pawn.NameStringShort + ") Years to Wait: " + ((double)ticksLeft / (double)GenDate.TicksPerYear) + " | Target age: " + targetAge);
+                    }
+
+                    if (is21OrYounger)
+                    {
+                        restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks;
+                    }
+
+                    if (refuelable.FuelPercent < 0.10f)
+                    {
+                        Log.Message("Not enough Uranium to heal.");
+                    }
+
+                    // Remove all health-related injuries if they're younger than the targetAge.
+                    if (hasInjuries && refuelable.FuelPercent >= 0.10f && restoreCoolDown > -1000 && pawn.ageTracker.AgeBiologicalTicks <= restoreCoolDown)
+                    {
+                        Log.Message("Fuel count to fully refueled: " + refuelable.GetFuelCountToFullyRefuel());
+                        Log.Message("Cooled down");
+
+                        string hediffName;
+                        foreach (Hediff oldHediff in pawn.health.hediffSet.GetHediffs<Hediff>().ToList())
                         {
-                            Log.Message("Cooled down");
-                            string hediffName;
-                            foreach (Hediff oldHediff in pawn.health.hediffSet.GetHediffs<Hediff>().ToList())
+                            hediffName = oldHediff.def.label;
+                            refuelable.ConsumeFuel(1f);
+
+                            if (hediffName == "gunshot")
                             {
-                                hediffName = oldHediff.def.label;
-
-                                if (hediffName == "gunshot")
-                                {
-                                    pawn.health.RemoveHediff(oldHediff);
-                                }
-                                else
-                                {
-                                    pawn.health.RemoveHediff(oldHediff);
-                                }
-
-                                restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks - GenDate.TicksPerYear;
-                                //restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks - GenDate.TicksPerSeason;
-                                Log.Message("Cured HEDIFF: " + hediffName);
-                                
-                                break;
+                                pawn.health.RemoveHediff(oldHediff);
                             }
+                            else
+                            {
+                                pawn.health.RemoveHediff(oldHediff);
+                            }
+
+                            restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks - GenDate.TicksPerYear;
+                            //restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks - GenDate.TicksPerSeason;
+                            Log.Message("Cured HEDIFF: " + hediffName + " @ " + oldHediff.def.description + " | " + oldHediff.ToString());
+
+                            break;
                         }
+                    }
+
+                    if (hasInjuries && (ticksLeft <= 0 || restoreCoolDown == -1000))
+                    {
+                        int ticksToWait = this.CalculateHealingTime(pawn);
+
+                        restoreCoolDown = pawn.ageTracker.AgeBiologicalTicks - ticksToWait;
+
+                        targetAge = (double)restoreCoolDown / (double)GenDate.TicksPerYear;
+                        Log.Message("Current Age in Ticks: " + pawn.ageTracker.AgeBiologicalTicks + " vs. " + restoreCoolDown);
+                        Log.Message("(" + pawn.NameStringShort + ") Years to Wait: " + ((double)ticksToWait / (double)GenDate.TicksPerYear) + " | Target age: " + targetAge);
                     }
                 }
 
@@ -127,7 +218,7 @@ namespace CryoRegenesis
                     power.PowerOutput = -props.basePowerConsumption;
                     if (power.PowerOn)
                     {
-                        refuelable.ConsumeFuel(fuelConsumption / GenDate.TicksPerYear);
+                        refuelable.ConsumeFuel(fuelConsumption);
                         cryptoHediffCooldown = Math.Max(cryptoHediffCooldown - 1, 0);
                         if (pawn.ageTracker.AgeBiologicalTicks > GenDate.TicksPerYear * 21)
                             pawn.ageTracker.AgeBiologicalTicks = Math.Max(pawn.ageTracker.AgeBiologicalTicks - rate, GenDate.TicksPerYear * 21);
@@ -216,6 +307,7 @@ namespace CryoRegenesis
             if (base.TryAcceptThing(thing, allowSpecialEffects))
             {
                 cryptoHediffCooldown = cryptoHediffCooldownBase;
+                restoreCoolDown = -1000;
                 enterTime = Find.TickManager.TicksGame;
                 if (refuelable.HasFuel && (AgeHediffs(thing as Pawn) > 0 || (thing as Pawn).ageTracker.AgeBiologicalTicks > GenDate.TicksPerYear * 21))
                 {
